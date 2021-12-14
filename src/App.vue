9daeccecf56a09c4fbccec845fdc5cd1dc501e49
Mat <mathew.teague@yewtreeweb.co.uk>
2021-12-14 16:19:50 +0000

    fetching covid data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="loading" v-if="loading">Loading data...</div>
+  <Header />
+  <main></main>
+  <Footer />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Footer
+  },
+  data () {
+    return {
+      loading: true,
+      date: null,
+      global: null,
+      countries: []
+    }
+  },
+  methods: {
+    async getCovidData (query = '') {
+      const api = `https://api.covid19api.com/${query}`
+      const response = await fetch(api)
+      const data = await response.json()
+      return data
+    }
+  },
+  created () {
+    this.getCovidData('summary')
+      .then(data => {
+        if (process.env.NODE_ENV !== 'production') {
+          console.log(data)
+        }
+        this.date = data.Date
+        this.global = data.Global
+        this.countries = data.Countries
+        this.loading = false
+      })
+      .catch(err => console.error(err))
+    this.getCovidData('live/country/united-kingdom/status/confirmed')
+      .then(data => {
+        if (process.env.NODE_ENV !== 'production') {
+          console.log(data)
+        }
+        // this.date = data.Date
+        // this.global = data.Global
+        // this.countries = data.Countries
+        this.loading = false
+      })
+      .catch(err => console.error(err))
   }
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
