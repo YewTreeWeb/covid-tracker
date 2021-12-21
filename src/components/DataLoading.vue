feff9c214c9aa673757cf26e2780f1e6dde60909
Mat <mathew.teague@yewtreeweb.co.uk>
2021-12-21 13:28:46 +0000

    customised svgs and added styling

---
+++ src/components/DataLoading.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="loader">
+    <div class="balls">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataLoading'
+}
+</script>
+
+<style lang="scss" scoped>
+.loader {
+  @include padding(em(60) em(40) null);
+  .balls {
+    width: 4em;
+    @include flex(center, space-between, row, false);
+    div {
+      @include size(0.8em);
+      border-radius: 50%;
+      background-color: v(clr-primary-100);
+    }
+    div:nth-of-type(1) {
+      transform: translateX(-100%);
+      animation: left-swing 0.5s ease-in alternate infinite;
+    }
+    div:nth-of-type(3) {
+      transform: translateX(-95%);
+      animation: right-swing 0.5s ease-out alternate infinite;
+    }
+  }
+}
+@keyframes left-swing {
+  50%,
+  100% {
+    transform: translateX(95%);
+  }
+}
+
+@keyframes right-swing {
+  50% {
+    transform: translateX(-95%);
+  }
+  100% {
+    transform: translateX(100%);
+  }
+}
+</style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="loading">
+    <div class="loader"></div>
+    <h2>Getting Covid-19 data...</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  position: relative;
+  z-index: 10;
+  @include size(100%, 100vh);
+  @include flex(center, center, column);
+  @supports (gap: 1px) {
+    gap: 4em;
+  }
+  > .loader {
+    position: relative;
+    @include size(4em);
+    border: 3px solid v(clr-primary-100);
+    overflow: hidden;
+    animation: spin 3s ease infinite;
+    &::before {
+      content: '';
+      position: absolute;
+      top: -3px;
+      left: -3px;
+      @include size(4em);
+      background-color: v(clr-primary-200);
+      transform-origin: center bottom;
+      transform: scaleY(1);
+      animation: fill 3s linear infinite;
+    }
+  }
+  > h2 {
+    margin-top: 2em;
+    @supports (gap: 1px) {
+      margin-top: unset;
+    }
+  }
+}
+
+@keyframes spin {
+  50%,
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes fill {
+  25%,
+  50% {
+    transform: scaleY(0);
+  }
+  100% {
+    transform: scaleY(1);
+  }
+}
+</style>
