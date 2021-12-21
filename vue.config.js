f2ccaf1250db725d38fd5c87e4426546a3128b60
Mat <mathew.teague@yewtreeweb.co.uk>
2021-12-21 10:16:33 +0000

    added loaderOptions

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  lintOnSave: process.env.NODE_ENV !== 'production'
-  // css: {
-  //   preprocessorOptions: {
-  //     scss: {
-  //       additionalData: `
-  //       @import "./src/styles/helpers/functions";
-  //       @import "./src/styles/helpers/variables";
-  //       @import "./src/styles/helpers/mixins";
-  //       `
-  //     }
-  //   }
-  // }
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+        @import "./src/styles/helpers/functions";
+        @import "./src/styles/helpers/variables";
+        @import "./src/styles/helpers/mixins";
+        `
+      }
+    }
+  }
 }
